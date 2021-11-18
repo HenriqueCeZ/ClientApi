ea701f206733fce8e7897671e52daed94b134f5e
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-18 20:02:42 -0300

    feat: implement client model

---
+++ src/main/java/com/example/springboot/model/Client.java
@@ -0,0 +1,35 @@
+package com.example.springboot.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity; //dizer que é uma entidade jpa
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate registerDate;
+
+
+    public static void main(String[] args) {
+        Client c = new Client();
+        
+    }
+}
