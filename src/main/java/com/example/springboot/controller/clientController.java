2a0a783368ef8f80d9f19f94a1c091a8d97b22c8
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-19 16:02:01 -0300

    feat: add requestbody in method save client

--- src/main/java/com/example/springboot/controller/clientController.java
+++ src/main/java/com/example/springboot/controller/clientController.java
@@ -6,6 +6,7 @@ import com.example.springboot.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class clientController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Client save(Client client){
+    public Client save(@RequestBody Client client){
         return repository.save(client);
     }
 
