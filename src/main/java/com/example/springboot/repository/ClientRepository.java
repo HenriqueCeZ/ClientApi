187bfc1f4ff964d784c749d45553c0d70948419d
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-19 14:51:37 -0300

    fix: fix imports

--- src/main/java/com/example/springboot/repository/ClientRepository.java
+++ src/main/java/com/example/springboot/repository/ClientRepository.java
@@ -2,7 +2,7 @@ package com.example.springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import ch.qos.logback.core.net.server.Client;
+import com.example.springboot.model.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Integer> {
     
--- src/main/java/com/example/springboot/repository/ServiceRepository.java
+++ src/main/java/com/example/springboot/repository/ServiceRepository.java
@@ -1,7 +1,9 @@
 package com.example.springboot.repository;
 
+import com.example.springboot.model.Service;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServiceRepository extends JpaRepository<Client, Integer> {
+public interface ServiceRepository extends JpaRepository<Service, Integer> {
     
 }
