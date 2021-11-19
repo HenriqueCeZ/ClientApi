641095047d725c0f6f4cac1887f00fd46960c7e8
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-19 14:16:49 -0300

    feat: implement model service

---
+++ src/main/java/com/example/springboot/model/Service.java
@@ -0,0 +1,36 @@
+package com.example.springboot.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ManyToAny;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length =150)
+    private String description;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_client")
+    private Client client;
+
+    @Column
+    private BigDecimal valor;
+    
+}
