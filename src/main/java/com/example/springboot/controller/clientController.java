96c36ac8d067b235bbc115df5181a0a70f0c8cdf
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-19 16:31:29 -0300

    feat: implement method client delete

--- src/main/java/com/example/springboot/controller/clientController.java
+++ src/main/java/com/example/springboot/controller/clientController.java
@@ -5,6 +5,7 @@ import com.example.springboot.repository.ClientRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,17 @@ public class clientController {
     @GetMapping("{id}")
     public Client searchClient(@PathVariable Integer id){
         return repository.findById(id).orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("{id} ")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        // repository.deleteById(id); um delete sem verificação
+         repository.findById(id).map(client ->{
+             repository.delete(client);
+             return Void.TYPE;
+         }).orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+   
     }
 
 }
