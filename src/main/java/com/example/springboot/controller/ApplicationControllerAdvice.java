f13677370c4ad91df5a3e4118e006a2438264c5b
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-20 15:53:04 -0300

    feat: creating a cleaner error-response driver

---
+++ src/main/java/com/example/springboot/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.example.springboot.controller;
+
+import java.util.stream.Collectors;
+
+import com.example.springboot.controller.exception.ApiErrors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Object handleValidationErros(MethodArgumentNotValidException ex){
+        BindingResult bindingResult = ex.getBindingResult();
+       List<String> messages = bindingResult.getAllErrors().stream().map(objectError -> objectError.getDefaultMessage()).collect(Collectors.toList());
+       return new ApiErrors(messages);
+    }
+
+}
