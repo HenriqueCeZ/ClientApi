4c2ba4442470aba1d6e7992598191b884b627d58
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-03 01:12:38 -0300

    feat: implemetando o serviceController e o primeiro método post

---
+++ src/main/java/com/example/springboot/controller/ServiceController.java
@@ -0,0 +1,50 @@
+package com.example.springboot.controller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+import com.example.springboot.controller.dto.ServiceDTO;
+import com.example.springboot.model.Client;
+import com.example.springboot.model.Service;
+import com.example.springboot.repository.ClientRepository;
+import com.example.springboot.repository.ServiceRepository;
+import com.example.springboot.util.BigDecimalConvert;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/services")
+@RequiredArgsConstructor
+public class ServiceController {
+    private final ClientRepository clientRepository;
+    private final ServiceRepository repository;
+    private final BigDecimalConvert bigDecimalConvert;
+
+
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) 
+    public Service save(@RequestBody ServiceDTO dto){
+       LocalDate date = LocalDate.parse(dto.getData(),DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+       Integer idClient = dto.getIdCliente();
+       Client client = clientRepository.findById(idClient).orElseThrow(()-> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente inexistente"));
+        Service service = new Service();
+        service.setDescription(dto.getDescricao());
+        service.setData(date);
+        service.setClient(client);
+        service.setValor(bigDecimalConvert.converter(dto.getPreco()));
+
+
+        return repository.save(service);
+    }
+    
+}
