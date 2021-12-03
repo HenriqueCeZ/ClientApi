66e4e84eafc67b36abf7aa275e59eb2687629485
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-03 03:02:41 -0300

    feat: implementando a query de busca serviço client e mês

--- src/main/java/com/example/springboot/repository/ServiceRepository.java
+++ src/main/java/com/example/springboot/repository/ServiceRepository.java
@@ -1,9 +1,20 @@
 package com.example.springboot.repository;
 
+
+import java.util.List;
+
 import com.example.springboot.model.Service;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ServiceRepository extends JpaRepository<Service, Integer> {
+
+    
+    @Query(" select s from Service s join s.client c " +
+    " where upper( c.name ) like upper( :nome ) and MONTH(s.data) =:mes")
+    List<Service> findByNomeClienteAndMes(
+    @Param("nome") String nome, @Param("mes") Integer mes);
     
 }
