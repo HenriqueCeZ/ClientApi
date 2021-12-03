bc725f6c0085d88882bea0aaeaa68955edcfb18a
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-03 01:15:22 -0300

    feat: implementando a classe bigDecimalConvert e o método converter

---
+++ src/main/java/com/example/springboot/util/BigDecimalConvert.java
@@ -0,0 +1,20 @@
+package com.example.springboot.util;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BigDecimalConvert {
+
+
+    public BigDecimal converter(String value){
+
+        if (value== null){
+            return null; 
+        }
+        value = value.replace(".", "").replace(",",".");
+        return new BigDecimal(value);
+    }
+    
+}
