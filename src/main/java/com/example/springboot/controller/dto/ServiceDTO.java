8aee94122874c7376f92f4aad3ed0a0de90c2d8f
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-03 01:13:46 -0300

    feat: implementando serviceDTO

---
+++ src/main/java/com/example/springboot/controller/dto/ServiceDTO.java
@@ -0,0 +1,16 @@
+package com.example.springboot.controller.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor 
+public class ServiceDTO {
+    private String descricao;
+    private String preco;
+    private String data;
+    private Integer idCliente;
+   
+   
+    
+}
