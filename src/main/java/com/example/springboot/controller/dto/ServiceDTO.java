12931ae8333350c3eda77181ddb882fd776c9ba7
HenriqueCeZ <henriquecezar.jp@gmail.com>
2022-02-09 22:19:48 -0300

    feat: implementando as mensagens de erros

--- src/main/java/com/example/springboot/controller/dto/ServiceDTO.java
+++ src/main/java/com/example/springboot/controller/dto/ServiceDTO.java
@@ -1,14 +1,23 @@
 package com.example.springboot.controller.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor 
 public class ServiceDTO {
+    @NotEmpty(message = "{campo.description.obrigatorio}")
     private String descricao;
+
+    @NotEmpty(message = "{campo.preco.obrigatorio}")
     private String preco;
+
+    @NotEmpty(message = "{campo.data.obrigatorio}")
     private String data;
+
+    @NotEmpty(message = "{campo.cliente.obrigatorio}")
     private Integer idCliente;
    
 }
